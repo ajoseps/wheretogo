f318c286a75bc396da159f83e33b3b6bacc45bd5
Arun Joseph <ajoseps@gmail.com>
2014-08-27 21:48:32 -0700

    got unique sessions but chat does not display fuck you chat

--- wtg.js
+++ wtg.js
@@ -1,16 +1,25 @@
-var myDataRef = new Firebase('https://tbz71x63fad.firebaseio-demo.com/');
+var myDataRef = new Firebase('https://sizzling-torch-2010.firebaseio.com/');
 var url = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBYZHhfGSwRMN1vFWDi2r2mnc-D09jwDDk";
+var session = '';
 
 $(document).ready(function() {
+  session = myDataRef.push({conversation:{}, query:"&q=Palo+Alto"});
+  session.child("conversation").push({name:"WTG",text:"Welcome!"});
+  session = session.name();
+  myDataRef = myDataRef.child(session);
+
+  // Initalizes Map
   myDataRef.child("query").on('value', function(snapshot){
     updateMap(snapshot);
   });
 });
 
+// Listener for map updates
 myDataRef.child("query").on('child_changed', function(snapshot){
   updateMap(snapshot);
 });
 
+// Stores search input into session's query slot (location) once entered
 $("#searchInput").keypress(function (e){
   if(e.keyCode == 13) {
     var params = "&q=" + $('#searchInput').val().replace(/ /g, "+");
@@ -21,6 +30,7 @@ $("#searchInput").keypress(function (e){
   }        
 });
 
+// Stores message into session's conversation once entered
 $('#messageInput').keypress(function (e) {
   if (e.keyCode == 13) {
     var name = $('#nameInput').val();
@@ -30,19 +40,22 @@ $('#messageInput').keypress(function (e) {
   }
 });
 
+// Listener for new messages
 myDataRef.child("conversation").on('child_added', function(snapshot) {
   var message = snapshot.val();
+  console.log("NEW: " + message[0]);
   displayChatMessage(message.name, message.text);
 });
 
+// Displays messages
 function displayChatMessage(name, text) {
   $('<li/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesOutputDiv'));
   $('#messagesOutputDiv')[0].scrollTop = $('#messagesOutputDiv')[0].scrollHeight;
 };
 
+// Update's the map's image
 function updateMap(snapshot){
   var changedQuery = snapshot.val();
-  console.log(changedQuery);
   var srcurl = url + changedQuery;
   var mapHtml = '<iframe id="map" width="100%" height="100%" frameborder="0" style="border:0" src=' + srcurl + '> </iframe>'
   $('#map').replaceWith(mapHtml);
