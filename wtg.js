32b3850133e4ff5206d74e110922d46c84c35c68
Arun Joseph <ajoseps@gmail.com>
2014-08-29 23:57:59 -0700

    has basic functionality chat is buggy and bootstrap panels do not display

--- wtg.js
+++ wtg.js
@@ -1,28 +1,29 @@
 var myDataRef = new Firebase('https://sizzling-torch-2010.firebaseio.com/');
 var url = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBYZHhfGSwRMN1vFWDi2r2mnc-D09jwDDk";
+var currenUrl = "http://127.0.0.1:8000/room.html"
 var session = '';
 
+
 $(document).ready(function() {
-  session = myDataRef.push({query:"&q=Palo+Alto"});
-  myDataRef = session;
+  var roomID = getParameterByName('roomID');
+  doesRoomExist(roomID);
 
   // Initalizes Map
   myDataRef.child("query").on('value', function(snapshot){
     updateMap(snapshot);
   });
 
+  // Listener for new messages
+  myDataRef.child("conversation").on('child_added', function(snapshot){
+    var message = snapshot.val();
+    displayChatMessage(message.name, message.text);
+  });
+
 });
 
-// Listener for map updates
-myDataRef.child("query").on('child_changed', function(snapshot){
-  updateMap(snapshot);
-});
-
-// Listener for new messages
-myDataRef.child("conversation").on('child_added', function(snapshot){
-  console.log("ADDED CHILD!");
-  var message = snapshot.val();
-  displayChatMessage(message.name, message.text);
+// Listener for 'Share Link' button click
+$("#shareButton").click(function(){
+  copyToClipboard();
 });
 
 // Stores search input into session's query slot (location) once entered
@@ -58,4 +59,64 @@ function updateMap(snapshot){
   var srcurl = url + changedQuery;
   var mapHtml = '<iframe id="map" width="100%" height="100%" frameborder="0" style="border:0" src=' + srcurl + '> </iframe>'
   $('#map').replaceWith(mapHtml);
+}
+
+// Checks if the room of specified id exists in firebase
+function doesRoomExist(id){
+  if(id == '')
+    loadRoom(false);
+  var base = new Firebase('https://sizzling-torch-2010.firebaseio.com/');
+  base.child(id).once('value', function(snapshot){
+    var exists = (snapshot.name() !== null);
+    loadRoom(exists, id);
+  })
+}
+
+// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript?page=1&tab=votes#tab-top
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+// Loads new or pre-existing room accordingly
+function loadRoom(exists, roomID){
+  if(!exists){
+    session = myDataRef.push({conversation:{},query:"&q=Sunnyvale"});
+    myDataRef = session;
+    session = session.name();
+  }
+  else{
+    session = new Firebase(myDataRef + "/" + roomID);
+    myDataRef = session;
+    session = roomID;
+
+    // Load Existing Map
+    myDataRef.child("query").once('value', function(snapshot){
+      updateMap(snapshot);
+    })
+
+    // Load Existing Messages
+    myDataRef.child("conversation").once('child_added', function(snapshot){
+      var message = snapshot.val();
+      displayChatMessage(message.name, message.text);
+    });
+  }
+
+  // Initalizes Map
+  myDataRef.child("query").on('value', function(snapshot){
+    updateMap(snapshot);
+  });
+
+  // Listener for new messages
+  myDataRef.child("conversation").on('child_added', function(snapshot){
+    var message = snapshot.val();
+    displayChatMessage(message.name, message.text);
+  });
+}
+
+// Prompts user to copy link to current room to clipboard to share
+function copyToClipboard() {
+  window.prompt("Copy Link to Room: Ctrl+C, Enter", currenUrl + "?roomID=" + session);
 }
